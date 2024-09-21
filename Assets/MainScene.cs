8f5203f3e69e9b183a62df8a9710bec1e1c14ab1
CS18701-PC\Dao <bocnoc2@gmail.com>
2024-09-21 14:39:09 +0900

    add mainscene select

--- Assets/MainScene.cs
+++ Assets/MainScene.cs
@@ -10,12 +10,16 @@ public class MainScene : MonoBehaviour
     private Button showLoveButton;
     private Button hideLoveButton;
     private RectTransform loveMessage;
+    private RectTransform wrongAnswer;
+    private RectTransform rightAnswer;
 
     void Awake()
     {
         showLoveButton =  gameObject.GetComponentsInChildren<Button>(true).First(x => x.name.Contains("Show"));
         hideLoveButton =  gameObject.GetComponentsInChildren<Button>(true).First(x => x.name.Contains("Hide"));
         loveMessage = gameObject.GetComponentsInChildren<RectTransform>(true).First(n => n.name.Contains("BorderHelloMessage"));
+        wrongAnswer = gameObject.GetComponentsInChildren<RectTransform>(true).First(n => n.name.Contains("WrongAnswer"));
+        rightAnswer = gameObject.GetComponentsInChildren<RectTransform>(true).First(n => n.name.Contains("RightAnswer"));
     }
 
     // Start is called before the first frame update
@@ -23,12 +27,14 @@ public class MainScene : MonoBehaviour
     {
         showLoveButton.OnClickAsObservable().Subscribe(_ =>
         {
-            loveMessage.gameObject.SetActive(true);
+            wrongAnswer.gameObject.SetActive(false);
+            rightAnswer.gameObject.SetActive(true);
         }).AddTo(this);
 
         hideLoveButton.OnClickAsObservable().Subscribe(_ =>
         {
-            loveMessage.gameObject.SetActive(false);
+            wrongAnswer.gameObject.SetActive(true);
+            rightAnswer.gameObject.SetActive(false);
         }).AddTo(this);
     }
 
