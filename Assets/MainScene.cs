07e81f266046d20aa4a5d60a50f4e86b42d65f45
CS18701-PC\Dao <bocnoc2@gmail.com>
2024-09-21 12:17:09 +0900

    フォントの追加

--- Assets/MainScene.cs
+++ Assets/MainScene.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScene : MonoBehaviour
 {
+    public Button showLoveButton;
+    public Button hideLoveButton;
+    public RectTransform loveMessage;
+
+    void Awake()
+    {
+        showLoveButton =  gameObject.GetComponentsInChildren<Button>(true).First(x => x.name.Contains("Show"));
+        hideLoveButton =  gameObject.GetComponentsInChildren<Button>(true).First(x => x.name.Contains("Hide"));
+        loveMessage = gameObject.GetComponentsInChildren<RectTransform>(true).First(n => n.name.Contains("BorderHelloMessage"));
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        showLoveButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            loveMessage.gameObject.SetActive(true);
+        }).AddTo(this);
+
+        hideLoveButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            loveMessage.gameObject.SetActive(false);
+        }).AddTo(this);
     }
 
     // Update is called once per frame
@@ -15,4 +37,6 @@ public class MainScene : MonoBehaviour
     {
         
     }
+
+
 }
