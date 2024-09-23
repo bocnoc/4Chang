ed443624be6aa2614733db9ed4174e045f944ee6
LAPTOP-3AKC5U57\dao <dao@morson.jp>
2024-09-23 09:37:25 +0900

    make instance initializer

---
+++ Assets/Models/InitializeApplicationController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+namespace AppSystemManager
+{
+    public static class InitializingApplicationController
+    {
+        //アプリケーション起動直前に呼び出され、初期化を行う。
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void OnApplicationStart()
+        {
+            var obj = new GameObject("InitAppComponent");
+            obj.AddComponent<SingletonInstanceMaker>();
+        }
+    }
+}
