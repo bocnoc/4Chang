8de1b5e3e8331c492e0042702db981b2d8c655a4
LAPTOP-3AKC5U57\dao <dao@morson.jp>
2024-09-26 17:21:35 +0900

    add side menu model

---
+++ Assets/Model/FlirtingDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+namespace Model
+{
+    public class FlirtingDatabase
+    {
+        public static List<string> flirtingList = new List<string>
+        {
+            "123",
+            "234",
+            "3456"
+        };
+    }
+}
---
+++ Assets/Model/SideMenuModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine.Assertions;
+namespace Model
+{
+    public class SideMenuModel
+    {
+        private static SideMenuModel _instance;
+        
+        public static SideMenuModel Instance
+        {
+            get => _instance ?? throw new ArgumentNullException();
+            set
+            {
+                Assert.IsNull(_instance, "You are not allowed to assign to a generated instance");
+                _instance = value;
+            }
+        }
+    }
+}
