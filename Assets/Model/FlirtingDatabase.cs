cd27d260e75704f1bacb9bff34d1518b02c8f792
LAPTOP-3AKC5U57\dao <dao@morson.jp>
2024-10-02 11:22:42 +0900

    database update

--- Assets/Model/FlirtingDatabase.cs
+++ Assets/Model/FlirtingDatabase.cs
@@ -5,9 +5,57 @@ namespace Model
     {
         public static List<string> flirtingList = new List<string>
         {
-            "123",
-            "234",
-            "3456"
+	        "	Lúc hôn e cứ như uống thuốc vậy - phải đủ LIỀU",
+	        "Anh gặp e muốn chết, cần phải đi CT(city) mới được",
+	        "Gọi e là ớt, a tập gym cũng vì cảm thấy yeu ot",
+	        "Uống có hết bao nhiêu can bia can rượu, thì cũng can’t get you out of my head",
+	        "E đau họng vì bụi ở VN cũng bởi vì e có quá nhiều sức húttt",
+	        "Em cứ như dầu gội vậy, vì bên em, e làm anh mất “chí” ",
+	        "Lúc nào a cũng thích văn vở, đơn giản thôi vì e đẹp như thơ",
+	        "Càng ko dc gặp em, anh lại càng phải đi chạy bộ, vì a cảm thấy mình “quá khổ”",
+	        "Trông như bánh mì vậy, vì trông e được phết",
+	        "Chắc e muốn ăn, vì a “đủ chất” và “ngon zai”",
+	        "Lâu lâu a cứ tưởng tình y mình là lọ 梅 a ngâm, cứ như là mơ vậy",
+	        "Với e lúc nào a cũng muốn dc có con, đó là connection",
+	        "E thích mì, dịch là gì? You love mi!",
+	        "Dạo này a bớt tra google rồi, vì ở e có tất cả những gì a tìm kiếm",
+	        "Có e xong a tự tin hơn cả Lê Lợi, vì ko cần kiếm nữa",
+	        "E giống như đồ án tốt nghiệp vậy, là thứ a cần bảo vệ ",
+	        "Thiếu e a cảm thấy như thiểu điều hoà, vì a cảm thấy mất mát",
+	        "Nhớ e cũng như thiếu máu vậy, vì lúc đó là lúc cần “thiết”",
+	        "A có phải sân bay đâu, mà Thiên Thần như e gặp a là cứ phải “cất cánh”",
+	        "E biết đường gì ngọt mà ko hại ko? Đường vào tim e đó",
+	        "E định làm gì đây, ngoài làm định mệnh của a",
+	        "Không phải tập gym, mà càng nghỉ a càng tăng cơ, tăng cơ hội gặp e",
         };
+
+	    public static List<string> flirtingListOld = new List<string>()
+	    {
+		    "Khi ở bên e a cứ như nha sĩ chưa tốt nghiệp…vì chẳng biết mần răng",
+		    "Tư nhiên thèm cá kho tương… chạy đi mua đồ thì nhớ ra mình còn hẳn 1 tấn tương tư",
+		    "A khong co em gai, ma sao lai thuong thay nho em",
+		    "Nhiệt độ tan chảy của tim a là nụ cười của e",
+		    "Oich, how can you be this hot",
+		    "Quen e giống như thấy nụ cười của trẻ sơ sinh..chỉ thấy có lợi",
+		    "Gọi e là kính mắt, vì cứ ở bên e là mắt sáng",
+		    "E có phải ảo thuật gia ko? Sao mỗi lần nhìn thấy e, mọi thứ khác đều biến mất",
+		    "Ốm ko cần cai vì chỉ cần cái ôm",
+		    "Nhà e có bán thịt bò ko? Mà vào a chỉ thấy すき",
+		    "Lẽ ra anh nên tên Hoàng, vì lúc nào trông e cũng như “bà Hoàng”",
+		    "Lên nhà e, a sợ làm phiền ng khác, vì a thích bị 迷惑",
+		    "Ngồi bàn nhà e anh toàn ngồi ngược, vì anh muốn ngược lại “đợi bàn”",
+		    "Ty anh cho e cứ như lương cty a vậy - ko bh thay đổi",
+		    "Đừng hỏi a muốn ăn cháo gì, vì a cứ gặp e là phải lòng!",
+		    "Dành cho mẹ thì tặng hoa mân côi, dành cho e thì tặng sự “mân mể”",
+		    "Rượu bia uống mấy cũng tan/ ăn e có tí mê man cả đời",
+		    "Muốn gọi cho em ở vn, cứ như học vật lý, vì phải có nhiều CÔNG THỨC",
+		    "Đừng cãi nhau, lúc đó a sẽ ngừng thở, vì ko có 息",
+		    "Bánh bao, dimsum, chẳng có món hấp nào ngon bằng món “hấp”dẫn của e",
+		    "Anh chơi chứng khoán vì nó giống như yeuem, cứ lao đầu vào thôi mà không cần “nhiều lời”",
+		    "E thích Đạo Nguyễn hay đ*o tàn b",
+		    "Mấy nay e chắc “đao họng” lắm nhỉ…tại vì e “hong có đạo”",
+		    " Đi bên e thì e phải chỉ đường cho a nhé, vì cứ bên e là a thấy “楽”",
+		    "Hôm nay chắc em dậy sớm, chac phai hon a nhieu",
+	    };
     }
 }
